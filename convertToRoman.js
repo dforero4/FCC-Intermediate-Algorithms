e2d72f9f420d35b745f7b7ae0152951a7505f4b8
Daniel Forero <topmidfielder04@yahoo.com>
2017-07-22 02:36:15 -0500

    Create convertToRoman.js

---
+++ convertToRoman.js
@@ -0,0 +1,23 @@
+Roman Numeral Converter
+*************************************************************
+Convert the given number into a roman numeral.
+
+All roman numerals answers should be provided in upper-case.
+*************************************************************
+
+function convertToRoman(num) {
+  var arr = [];
+  var dec = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var rom = ["M","CM", "D" ,"CD" ,"C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+      
+  for(var i=0; i<dec.length; i++){
+    while(num%dec[i]<num){
+      arr.push(rom[i]);
+      num -= dec[i];
+    }
+  }
+  
+  return arr.join("");
+}
+
+convertToRoman(36);
