68a717c3d3b27d78626c1833cf3fc473f352dd16
Daniel Forero <topmidfielder04@yahoo.com>
2017-07-22 04:21:18 -0500

    Create steamrollArray.js

---
+++ steamrollArray.js
@@ -0,0 +1,20 @@
+Steamrollers
+************************************************************************
+
+Flatten a nested array. You must account for varying levels of nesting.
+
+************************************************************************
+
+function steamrollArray(arr) {
+  return flatten(arr);
+}
+
+function flatten(arr){
+  return arr.reduce(function(acc, val) {
+    return acc.concat(
+      Array.isArray(val) ? flatten(val) : val
+    );
+  },[]);
+} 
+
+steamrollArray([1, [], [3, [[4]]]]);
