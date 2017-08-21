20779bc3956f0a6f58e05e651c5573c103f9e853
Daniel Forero <topmidfielder04@yahoo.com>
2017-08-21 00:48:56 -0500

    Update findElement.js

--- findElement.js
+++ findElement.js
@@ -1,8 +1,8 @@
-Finders Keepers
-***********************************************************************************
+// Finders Keepers
+/***********************************************************************************
 Create a function that looks through an array (first argument) and 
 returns the first element in the array that passes a truth test (second argument).
-***********************************************************************************
+***********************************************************************************/
 
 function findElement(arr, func) {
   //This line filters the array based on the passed in function, then selects the first value.
