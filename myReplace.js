5617b86c733ae9096ccd7dc2c21268eefe06119a
Daniel Forero <topmidfielder04@yahoo.com>
2017-08-21 00:49:50 -0500

    Update myReplace.js

--- myReplace.js
+++ myReplace.js
@@ -1,5 +1,5 @@
-Search and Replace
-********************************************************************************************************
+// Search and Replace
+/********************************************************************************************************
 Perform a search and replace on the sentence using the arguments provided and return the new sentence.
 
 First argument is the sentence to perform the search and replace on.
@@ -10,7 +10,7 @@ Third argument is what you will be replacing the second argument with (after).
 
 NOTE: Preserve the case of the original word when you are replacing it. 
 For example if you mean to replace the word "Book" with the word "dog", it should be replaced as "Dog".
-********************************************************************************************************
+********************************************************************************************************/
 
 function myReplace(str, before, after) {
   var letter = before.charAt(0);
