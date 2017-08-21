e41926915bc16abbe37967bed09fab6e571b00f7
Daniel Forero <topmidfielder04@yahoo.com>
2017-08-21 00:52:46 -0500

    Update translatePigLatin.js

--- translatePigLatin.js
+++ translatePigLatin.js
@@ -1,5 +1,5 @@
-Pig Latin
-*************************************************************************************************************************************
+// Pig Latin
+/*************************************************************************************************************************************
 Translate the provided string to pig latin.
 
 Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
@@ -7,7 +7,7 @@ Pig Latin takes the first consonant (or consonant cluster) of an English word, m
 If a word begins with a vowel you just add "way" to the end.
 
 Input strings are guaranteed to be English words in all lowercase.
-*************************************************************************************************************************************
+*************************************************************************************************************************************/
 
 function translatePigLatin(str) {
   var notCons = ['a', 'e', 'i', 'o', 'u'];
