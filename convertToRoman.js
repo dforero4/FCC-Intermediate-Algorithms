91e269959cf2bb0b4d28601baa22995ac8cc0f2f
Daniel Forero <topmidfielder04@yahoo.com>
2017-08-21 00:47:25 -0500

    Update convertToRoman.js

--- convertToRoman.js
+++ convertToRoman.js
@@ -1,9 +1,9 @@
-Roman Numeral Converter
-*************************************************************
+// Roman Numeral Converter
+/*************************************************************
 Convert the given number into a roman numeral.
 
 All roman numerals answers should be provided in upper-case.
-*************************************************************
+*************************************************************/
 
 function convertToRoman(num) {
   var arr = [];
